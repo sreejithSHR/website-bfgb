b9902fd696f71ade695cc54744543342c7f9050e
salgue441 <carlossalguero441@gmail.com>
2024-07-09 16:55:54 -0600

    feat(main): updated clerkMiddleware to match subdomain routing through hostname. Additionally, updated the next.config.mjs to include the new remotePatterns attribute

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "uploadthing.com",
-      "utfs.io",
-      "img.clerk.com",
-      "subdomain",
-      "files.stripe.com"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uploadthing.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io'
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'files.stripe.com'
+      },
     ]
   },
-  reactStrictMode: false
+  reactStrictMode: false,
+  pageExtensions: ['tsx', 'ts'],
 };
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
 
-const isPublicRoute = createRouteMatcher(["/site", "/api/uploadthing"])
+const isPublicRoute = createRouteMatcher([
+  "/site",
+  "/api/uploadthing",
+  "/sign-in",
+  "/sign-up",
+])
 
 /**
  * Handles authentication and authorization with Clerk.
+ *
+ * Protects all routes except for the defined public routes.
+ * Redirects to the appropriate subdomain based on the hostname.
  *
  * @see https://clerk.com/docs/references/nextjs/clerk-middleware
  * @param auth: ClerkMiddlewareAuth - Clerk auth object
@@ -12,6 +21,34 @@ const isPublicRoute = createRouteMatcher(["/site", "/api/uploadthing"])
  * @returns NextMiddlewareReturn - Next middleware return
  */
 export default clerkMiddleware((auth, request) => {
+  const url = new URL(request.url)
+  const searchParams = url.searchParams.toString()
+  const hostname = request.headers.get("host")
+  const pathWithParams = `${url.pathname}${
+    searchParams ? `?${searchParams}` : ""
+  }`
+
+  const subDomain = hostname
+    ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
+    .filter(Boolean)[0]
+
+  if (subDomain) {
+    return NextResponse.rewrite(
+      new URL(`/${subDomain}${pathWithParams}`, request.url)
+    )
+  }
+
+  if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
+    return NextResponse.redirect(new URL(`agency${url.pathname}`, request.url))
+  }
+
+  if (
+    url.pathname === "/" ||
+    (url.pathname === "/site" && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+  ) {
+    return NextResponse.redirect(new URL(`/site`, request.url))
+  }
+
   if (!isPublicRoute(request)) {
     auth().protect()
   }
